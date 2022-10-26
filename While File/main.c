b5047ea7dfcc9b8551a668bb7b6f228e2bd8cc1f
Choooom <116259874+Choooom@users.noreply.github.com>
2022-10-26 19:26:32 +0800

    Add files via upload

--- While File/main.c
+++ While File/main.c
@@ -5,37 +5,37 @@
 
 int main() {
 
-    int studentCount = 0;
+    int const bscs_ya1_total_students = 6, bscs_ya2_total_students = 50, bsit_ya1_total_students = 50, bsit_ya2_total_students = 50;
+    int bscs_ya1_students = 0, bscs_ya2_students = 0, bsit_ya1_students = 0, bsit_ya2_students = 0;
     char course[50], section[50], last_name[50], first_name[50], vaccinationStatus[50], contactNumber[15];
-    int startOfclass = 1666580405, absentCutoff = 1666584005;
+    int startOfclass = 1666782633, absentCutoff = 1666782933;
     int endOfline = 0;
-    int totalStudents = 6, presentStudents = 0, lateStudents = 0;
+    int presentStudents = 0, lateStudents = 0;
     int absentStudents, timesUp = 0;
-    char c, b;
+    char presentStatus, absentStatus;
 
 
     FILE *fpointer;
 
-    fpointer  = fopen("BSCS_YA-1_Attendance_Sheet.txt", "w");
+    fpointer  = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "w");
         fprintf(fpointer, "%s\n", HEADER);
 	fclose(fpointer);
 
-    fpointer = fopen("BSCS_YA-2_Attendance_Sheet.txt", "w");
+    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "w");
         fprintf(fpointer, "%s\n", HEADER);
 	fclose(fpointer);
 
-    fpointer = fopen("BSIT_YA-1_Attendance_Sheet.txt", "w");
+    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "w");
         fprintf(fpointer, "%s\n", HEADER);
 	fclose(fpointer);
 
-    fpointer = fopen("BSIT_YA-2_Attendance_Sheet.txt", "w");
+    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "w");
         fprintf(fpointer, "%s\n", HEADER);
 	fclose(fpointer);
 
 
-	while(studentCount < 6 && timesUp != 1){
 
-    FILE *fpointer;
+	while(bscs_ya1_students < bscs_ya1_total_students && bscs_ya2_students < bscs_ya2_total_students && bsit_ya1_students < bsit_ya1_total_students && bsit_ya2_students < bsit_ya2_total_students && timesUp != 1){
 
             printf("Attendance Sheet\n");
             printf("\nEnter your course: ");
@@ -64,23 +64,23 @@ int main() {
 
                     printf("\nSorry, the Attendance Sheet has now closed.\nNext time, try waking up earlier :   ^)\n");
 
-                    fpointer = fopen("BSCS_YA-1_Attendance_Sheet.txt", "r");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "r");
 
-                        for (c = getc(fpointer); c != EOF; c = getc(fpointer))
-                            if (c == '^')
+                        for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                            if (presentStatus == '^')
                             presentStudents += 1;
                     fclose(fpointer);
 
-                    fpointer = fopen("BSCS_YA-1_Attendance_Sheet.txt", "r");
-                        for (b = getc(fpointer); b != EOF; b = getc(fpointer))
-                            if (b == '~')
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "r");
+                        for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                            if (absentStatus == '~')
                             lateStudents += 1;
 
                     fclose(fpointer);
 
-                    absentStudents = totalStudents - (presentStudents + lateStudents);
+                    absentStudents = bscs_ya1_total_students - (presentStudents + lateStudents);
 
-                    fpointer = fopen("BSCS_YA-1_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "a");
                         fprintf(fpointer, "\nSummary:\n");
                         fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
                         fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
@@ -91,7 +91,7 @@ int main() {
 
                 }else{
 
-                    fpointer = fopen("BSCS_YA-1_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "a");
 
                         fprintf(fpointer, "\nName: %s, %s", last_name, first_name);
                         fprintf(fpointer, "\nContact Number: %s", contactNumber);
@@ -106,6 +106,7 @@ int main() {
                             fprintf(fpointer, "\n");
 
                     fclose(fpointer);
+                    bscs_ya1_students++;
 
                     printf("\nYour response has been recorded.\nThank You.");
                 }
@@ -132,23 +133,22 @@ int main() {
 
                     printf("\nSorry, the Attendance Sheet has now closed.\nNext time, try waking up earlier :   ^)\n");
 
-                    fpointer = fopen("BSCS_YA-2_Attendance_Sheet.txt", "r");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "r");
 
-                        for (c = getc(fpointer); c != EOF; c = getc(fpointer))
-                            if (c == '^')
+                        for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                            if (presentStatus == '^')
                             presentStudents += 1;
                     fclose(fpointer);
 
-                    fpointer = fopen("BSCS_YA-2_Attendance_Sheet.txt", "r");
-                        for (b = getc(fpointer); b != EOF; b = getc(fpointer))
-                            if (b == '~')
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "r");
+                        for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                            if (absentStatus == '~')
                             lateStudents += 1;
-
                     fclose(fpointer);
 
-                    absentStudents = totalStudents - (presentStudents + lateStudents);
+                    absentStudents = bscs_ya2_total_students - (presentStudents + lateStudents);
 
-                    fpointer = fopen("BSCS_YA-2_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "a");
                         fprintf(fpointer, "\nSummary:\n");
                         fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
                         fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
@@ -159,7 +159,7 @@ int main() {
 
                 }else{
 
-                    fpointer = fopen("BSCS_YA-2_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "a");
 
                         fprintf(fpointer, "\nName: %s, %s", last_name, first_name);
                         fprintf(fpointer, "\nContact Number: %s", contactNumber);
@@ -174,6 +174,7 @@ int main() {
                             fprintf(fpointer, "\n");
 
                     fclose(fpointer);
+                    bscs_ya2_students++;
 
                     printf("\nYour response has been recorded.\nThank You.");
                 }
@@ -182,9 +183,9 @@ int main() {
                 system("cls");
 
             }else{
-                printf("Error! Invalid Section");
-		sleep(3);
-		system("cls");    
+                printf("\nError! Invalid Section");
+                sleep(3);
+                system("cls");
             }
 
         }else if(strcmp(strupr(course), "BSIT") == 0){
@@ -208,23 +209,22 @@ int main() {
 
                     printf("\nSorry, the Attendance Sheet has now closed.\nNext time, try waking up earlier :   ^)\n");
 
-                    fpointer = fopen("BSIT_YA-1_Attendance_Sheet.txt", "r");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "r");
 
-                        for (c = getc(fpointer); c != EOF; c = getc(fpointer))
-                            if (c == '^')
+                        for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                            if (presentStatus == '^')
                             presentStudents += 1;
                     fclose(fpointer);
 
-                    fpointer = fopen("BSIT_YA-1_Attendance_Sheet.txt", "r");
-                        for (b = getc(fpointer); b != EOF; b = getc(fpointer))
-                            if (b == '~')
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "r");
+                        for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                            if (absentStatus == '~')
                             lateStudents += 1;
-
                     fclose(fpointer);
 
-                    absentStudents = totalStudents - (presentStudents + lateStudents);
+                    absentStudents = bsit_ya1_total_students - (presentStudents + lateStudents);
 
-                    fpointer = fopen("BSIT_YA-1_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "a");
                         fprintf(fpointer, "\nSummary:\n");
                         fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
                         fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
@@ -235,7 +235,7 @@ int main() {
 
                 }else{
 
-                    fpointer = fopen("BSIT_YA-1_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "a");
 
                         fprintf(fpointer, "\nName: %s, %s", last_name, first_name);
                         fprintf(fpointer, "\nContact Number: %s", contactNumber);
@@ -250,6 +250,7 @@ int main() {
                             fprintf(fpointer, "\n");
 
                     fclose(fpointer);
+                    bsit_ya1_students++;
 
                     printf("\nYour response has been recorded.\nThank You.");
                 }
@@ -276,23 +277,23 @@ int main() {
 
                     printf("\nSorry, the Attendance Sheet has now closed.\nNext time, try waking up earlier :   ^)\n");
 
-                    fpointer = fopen("BSIT_YA-2_Attendance_Sheet.txt", "r");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "r");
 
-                        for (c = getc(fpointer); c != EOF; c = getc(fpointer))
-                            if (c == '^')
+                        for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                            if (presentStatus == '^')
                             presentStudents += 1;
                     fclose(fpointer);
 
-                    fpointer = fopen("BSIT_YA-2_Attendance_Sheet.txt", "r");
-                        for (b = getc(fpointer); b != EOF; b = getc(fpointer))
-                            if (b == '~')
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "r");
+                        for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                            if (absentStatus == '~')
                             lateStudents += 1;
 
                     fclose(fpointer);
 
-                    absentStudents = totalStudents - (presentStudents + lateStudents);
+                    absentStudents = bsit_ya2_total_students - (presentStudents + lateStudents);
 
-                    fpointer = fopen("BSIT_YA-2_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "a");
                         fprintf(fpointer, "\nSummary:\n");
                         fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
                         fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
@@ -303,7 +304,7 @@ int main() {
 
                 }else{
 
-                    fpointer = fopen("BSIT_YA-2_Attendance_Sheet.txt", "a");
+                    fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "a");
 
                         fprintf(fpointer, "\nName: %s, %s", last_name, first_name);
                         fprintf(fpointer, "\nContact Number: %s", contactNumber);
@@ -318,6 +319,7 @@ int main() {
                             fprintf(fpointer, "\n");
 
                     fclose(fpointer);
+                    bsit_ya2_students++;
 
                     printf("\nYour response has been recorded.\nThank You.");
                 }
@@ -326,15 +328,122 @@ int main() {
                 system("cls");
 
             }else{
-                printf("Error! Invalid Section");
-		sleep(3);
-		system("cls");     
+                printf("\nError! Invalid Section");
+                sleep(3);
+                system("cls");
             }
         }else{
-            printf("Error! Invalid Course!");
-	    sleep(3);
-	    system("cls");   
+            printf("\nError! Invalid Course!");
+            sleep(3);
+            system("cls");
         }
 	}
+
+	if(bscs_ya1_students == bscs_ya1_total_students){
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "r");
+            for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                if (presentStatus == '^')
+                    presentStudents += 1;
+        fclose(fpointer);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "r");
+            for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                if (absentStatus == '~')
+                    lateStudents += 1;
+        fclose(fpointer);
+
+        absentStudents = bscs_ya1_total_students - (presentStudents + lateStudents);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-1\\BSCS_YA-1_Attendance_Sheet.txt", "a");
+            fprintf(fpointer, "\nSummary:\n");
+            fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
+            fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
+            fprintf(fpointer, "\nNumber of Students Absent: %d", absentStudents);
+        fclose(fpointer);
+
+        printf("\nThe Attendace Sheet has now officially closed! Thank You!\n");
+
+	}else if(bscs_ya2_students == bscs_ya2_total_students){
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "r");
+
+            for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                if (presentStatus == '^')
+                presentStudents += 1;
+        fclose(fpointer);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "r");
+            for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                if (absentStatus == '~')
+                lateStudents += 1;
+        fclose(fpointer);
+
+        absentStudents = bscs_ya2_total_students - (presentStudents + lateStudents);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSCS 1-YA-2\\BSCS_YA-2_Attendance_Sheet.txt", "a");
+            fprintf(fpointer, "\nSummary:\n");
+            fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
+            fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
+            fprintf(fpointer, "\nNumber of Students Absent: %d", absentStudents);
+        fclose(fpointer);
+
+        printf("\nThe Attendace Sheet has now officially closed! Thank You!\n");
+
+	}else if(bsit_ya1_students == bsit_ya1_total_students){
+
+         fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "r");
+
+            for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                if (presentStatus == '^')
+                presentStudents += 1;
+        fclose(fpointer);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "r");
+            for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                if (absentStatus == '~')
+                lateStudents += 1;
+        fclose(fpointer);
+
+        absentStudents = bsit_ya1_total_students - (presentStudents + lateStudents);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-1\\BSIT_YA-1_Attendance_Sheet.txt", "a");
+            fprintf(fpointer, "\nSummary:\n");
+            fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
+            fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
+            fprintf(fpointer, "\nNumber of Students Absent: %d", absentStudents);
+        fclose(fpointer);
+
+        printf("\nThe Attendace Sheet has now officially closed! Thank You!\n");
+
+	}else if(bsit_ya2_students == bsit_ya2_total_students){
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "r");
+
+            for (presentStatus = getc(fpointer); presentStatus != EOF; presentStatus = getc(fpointer))
+                if (presentStatus == '^')
+                presentStudents += 1;
+        fclose(fpointer);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "r");
+            for (absentStatus = getc(fpointer); absentStatus != EOF; absentStatus = getc(fpointer))
+                if (absentStatus == '~')
+                lateStudents += 1;
+
+        fclose(fpointer);
+
+        absentStudents = bsit_ya2_total_students - (presentStudents + lateStudents);
+
+        fpointer = fopen("C:\\Users\\Romille Ilaida\\Desktop\\Attendance Folder\\BSIT 1-YA-2\\BSIT_YA-2_Attendance_Sheet.txt", "a");
+            fprintf(fpointer, "\nSummary:\n");
+            fprintf(fpointer, "\nNumber of Students Present: %d", presentStudents);
+            fprintf(fpointer, "\nNumber of Students Late: %d", lateStudents);
+            fprintf(fpointer, "\nNumber of Students Absent: %d", absentStudents);
+        fclose(fpointer);
+
+        printf("\nThe Attendace Sheet has now officially closed! Thank You!\n");
+
+	}
+
 	return 0;
 }
